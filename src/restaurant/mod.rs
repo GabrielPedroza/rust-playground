eb17d03c25567057bd100afb589f95864b825902
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-09-30 16:25:33 -0400

    mods

---
+++ src/restaurant/mod.rs
@@ -0,0 +1,42 @@
+pub mod pizza_order {
+    pub struct Pizza {
+        pub dough: String,
+        pub sauce: String,
+        pub topping: String,
+    }
+
+    impl Pizza {
+        pub fn lunch(topping: &str) -> Pizza {
+            Pizza {
+                dough: String::from("thin"),
+                sauce: String::from("tomato"),
+                topping: String::from(topping),
+            }
+        }
+    }
+
+    pub mod help_customer {
+        fn seat_at_table() {
+            println!("Customer seated at table");
+        }
+
+        pub fn take_order() {
+            seat_at_table();
+
+            // super is needed because Pizza is defined in the parent module
+            let cust_pizza: super::Pizza = 
+                super::Pizza::lunch("veggies");
+
+            serve_customer(cust_pizza);
+        }
+
+        fn serve_customer(cust_pizza: super::Pizza){
+            println!("The customer is served a regular pizza with {}", cust_pizza.topping);
+        }
+
+    }
+}
+
+pub fn order_food() {
+    crate::restaurant::pizza_order::help_customer::take_order();
+}
