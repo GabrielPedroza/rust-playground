b6b59c49d8f7b4ab139aaa643d2ef5a0c8f9d2e7
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-10-11 20:23:41 -0400

    this took me a while to code out (understanding syntax and rust features mainly). not needed but ive learnt something at least

--- src/threads.rs
+++ src/threads.rs
@@ -1,22 +1,53 @@
 #![allow(dead_code)]
 
+// use std::ops::AddAssign;
 pub fn threads() {
-    use std::thread;
-    use std::time::Duration;
-
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("Hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    // handle.join().unwrap();
-
-    for i in 1..5 {
-        println!("Hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
+    pub trait Transaction {
+        fn as_f64(&self) -> f64;
     }
 
-    handle.join().unwrap();
+    impl Transaction for f64 {
+        fn as_f64(&self) -> f64 {
+            *self
+        }
+    }
+
+    impl Transaction for i64 {
+        fn as_f64(&self) -> f64 {
+            *self as f64
+        }
+    }
+
+    pub struct Bank {
+        balance: f64,
+    }
+
+    impl Bank {
+        pub fn new() -> Bank {
+            Bank { balance: 0.0 }
+        }
+
+        pub fn deposit<T: Transaction>(&mut self, amount: T) -> &mut Self {
+            self.balance += amount.as_f64();
+            self
+        }
+
+        pub fn withdraw<T: Transaction>(&mut self, amount: T) -> &mut Self {
+            self.balance -= amount.as_f64();
+            self
+        }
+
+        pub fn balance(&self) -> f64 {
+            self.balance
+        }
+    }
+
+    let mut bank = Bank::new();
+    bank.deposit(100.5);
+    bank.deposit(25);
+    bank.withdraw(100.5);
+    bank.withdraw(25);
+
+    println!("Balance: {}", bank.balance());
+
 }
